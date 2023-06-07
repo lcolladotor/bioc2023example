013867ecae8a753395d24b7fbd8280c93744def8
Leonardo Collado Torres <lcolladotor@gmail.com>
2023-06-07 11:44:15 -0400

    Add a little function. Ignore pkgdown dir for R CMD build

---
+++ R/hello.R
@@ -0,0 +1,12 @@
+#' Say hello
+#' 
+#' Just a short function that prints the word `hello`.
+#'
+#' @return A `character(1)` with the word `hello`.
+#' @export
+#'
+#' @examples
+#' hello()
+hello <- function() {
+    print("hello")
+}
